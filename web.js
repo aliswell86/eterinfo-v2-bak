fdf79b8564ddb32a7a1f84d04e39bb6af4a8b917
kdk <aliswell86@naver.com>
2018-12-20 12:31:18 +0900

    a

--- web.js
+++ web.js
@@ -4,10 +4,11 @@ const Koa = require('koa');
 const serve = require('koa-static');
 const path = require('path');
 const mongoose = require('mongoose');
+const fs = require('fs');
 
 const app = new Koa();
 
-
+const indexHtml = fs.readFileSync(path.resolve(__dirname, './views/build/index.html'), { encoding: 'utf8' });
 
 mongoose.Promise = global.Promise; // Node의 Promise를 사용하도록 설정
 mongoose.connect('mongodb://my_mean:dlskdud1@ds121321.mlab.com:21321/my_mean').then(() => {
